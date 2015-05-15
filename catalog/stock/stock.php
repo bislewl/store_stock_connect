94e37667ad79aefca059e885cdec3195e8e257c0
Larry Bislew <bislewl@gmail.com>
2015-05-15 14:54:27 -0500

    modified changes

--- catalog/stock/stock.php
+++ catalog/stock/stock.php
@@ -8,7 +8,7 @@ require_once('includes/application_top.php');
 $connection_type = zen_db_prepare_input($_GET['type']);
 $products_table = TABLE_PRODUCTS;
 
-$current_inventory = $db->Execute("SELECT * FROM " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_PRODUCTS . " p WHERE pd.products_id = p.products_id");
+$current_inventory = $db->Execute("SELECT * FROM " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_PRODUCTS . " p WHERE pd.products_id = p.products_id ORDER BY p.products_model");
 while (!$current_inventory->EOF) {
     $category = '';
     $cpath = zen_get_product_path((int)$current_inventory->fields['products_id']);
@@ -16,7 +16,7 @@ while (!$current_inventory->EOF) {
     foreach($cpath_array as $cat_id){
        $category .= '|'.zen_get_category_name((int)$cat_id,1);
     }
-    $category .= '|';
+    $category = ltrim($category, '|');
     $products[] = array(
         'products_name' => $current_inventory->fields['products_name'],
         'products_model' => $current_inventory->fields['products_model'],
@@ -26,11 +26,11 @@ while (!$current_inventory->EOF) {
         'wholesale_price' => number_format($current_inventory->fields['products_price'],2),
         'products_description' => htmlspecialchars($current_inventory->fields['products_description']),
         'products_category' => $category,
-      //  'msrp' => number_format($current_inventory->fields['products_msrp'],2),
-     //   'products_height' => $current_inventory->fields['products_height'],
-     //   'products_width' => $current_inventory->fields['products_width'],
-     //   'products_length' => $current_inventory->fields['products_length'],
-      //  'scale' => $current_inventory->fields['products_scale'],
+        'msrp' => number_format($current_inventory->fields['products_msrp'],2),
+        'products_height' => number_format($current_inventory->fields['products_height'],2),
+        'products_width' => number_format($current_inventory->fields['products_width'],2),
+        'products_length' => number_format($current_inventory->fields['products_length'],2),
+        'products_scale' => number_format($current_inventory->fields['products_scale'],2),
     );
     // echo $products['manufacturer'].'<br/>';
     $current_inventory->MoveNext();
@@ -57,8 +57,9 @@ switch ($connection_type) {
         break;
     case 'csv':
     default:
+        $filename = 'foltz_stock_'.date('Ymd_His').'.csv';
         header('Content-Type: text/csv; charset=utf-8');
-        header('Content-Disposition: attachment; filename=stock.csv');
+        header('Content-Disposition: attachment; filename='.$filename);
         $out = fopen('php://output', 'w');
         fputcsv($out, array_keys($products['0']));
         foreach ($products as $product) {
